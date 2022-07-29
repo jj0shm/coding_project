913f27ab1f6a636c8e8e237d11bd55f76e999ec0
Josh Miller <107601540+jj0shm@users.noreply.github.com>
2022-07-28 19:54:30 -0500

    Add files via upload

---
+++ codingproject.java
@@ -0,0 +1,12 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		String firstName = "Josh";
+		String lastName = "Miller";
+		String fullName = "Josh" + " "+ "Miller";
+		System.out.println(fullName);
+	}
+	
+}
