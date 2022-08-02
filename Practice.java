5103afe992f2880306ae88f9afee94905b8b76b3
Josh Miller <107601540+jj0shm@users.noreply.github.com>
2022-08-02 16:55:07 -0500

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,12 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		String names[] = {"sam" , "Tommy" , "Tim" , "Buck" , "Bob"};
+		
+		System.out.println(names[0] + " " + names[1] + " " + names[2] + " " + names[3] + " " + names[4]);
+		
+		
+	}
+}
